0aff68191565d633a84146e08d93644de862980d
igborre <iborreani@correo.um.edu.uy>
2024-05-26 17:15:57 -0300

    Update TestUnitariosStack.java

--- src/uy/edu/um/prog2/tad/stack/TestUnitariosStack.java
+++ src/uy/edu/um/prog2/tad/stack/TestUnitariosStack.java
@@ -1,4 +1,14 @@
 package uy.edu.um.prog2.tad.stack;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class TestUnitariosStack {
+    MyStack<Integer> stackTesteo = new Stack<>();
+
+    @Test
+    public void testInsert() {
+        stackTesteo.push(1);
+        stackTesteo.push(2);
+        stackTesteo.push(3);
+    }
 }
