25a742195c613d7f5baeadb871be2cde0c022a1b
igborre <iborreani@correo.um.edu.uy>
2024-05-27 10:52:36 -0300

    Get function Implementation

--- src/uy/edu/um/prog2/tad/hash/MyHashInterface.java
+++ src/uy/edu/um/prog2/tad/hash/MyHashInterface.java
@@ -3,6 +3,7 @@ package uy.edu.um.prog2.tad.hash;
 public interface MyHashInterface <K,V> {
 
     public void put(K key, V value);
+    public V getValue(K key);
     public boolean contains(K key);
     public void remove(K clave);
     public int capacity();
